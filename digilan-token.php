87bd3ff6dad901d3caecf6e2f528053bfad1d465
dohnuts <sven.falempin@gmail.com>
2021-01-19 14:19:28 -0500

    y php when you can txt

--- digilan-token.php
+++ digilan-token.php
@@ -1024,7 +1024,9 @@ class DigilanToken
 
     public static function getDigilanVersion()
     {
-        wp_die(self::$digilan_version, '', 200);
+        status_header( 200 );
+        echo self::$digilan_version;
+        die;
     }
 
     /*
