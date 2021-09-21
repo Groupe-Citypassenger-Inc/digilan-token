2c7ab5b2b95df8b27c55e968f8cd13d3cd95e516
jamesjin18 <maman1000000@hotmail.fr>
2021-09-21 16:19:46 +0200

    indentation

--- admin/templates/multi-portal.php
+++ admin/templates/multi-portal.php
@@ -26,67 +26,67 @@ if (preg_match($re, $secret) == 1) :
   );
   $loop = new WP_Query($args);
 ?>
-<div class="dlt-admin-content">
-    <h1><?php _e('Multiportal Configuration','digilan-token'); ?></h1>
+  <div class="dlt-admin-content">
+    <h1><?php _e('Multiportal Configuration', 'digilan-token'); ?></h1>
     <div id='digilan_token_link_ap_div'>
-        <h2><?php _e('Link User to an AP','digilan-token'); ?></h2>
-        <?php 
-        if (false == DigilanTokenMultiPortal::is_ap_available()) {
-        ?>
-            <p>
-                <?php _e('There are not ap available','digilan-token'); ?>
-            </p>
-        <?php
-        } else {
-        ?>
-            <form method="POST" action="<?php echo admin_url('admin-post.php'); ?>">
-                <?php wp_nonce_field('digilan-token-plugin'); ?>
-                <input type="hidden" name="digilan-token-link-ap" value="true" />
-                <input type="hidden" name="action" value="digilan-token-plugin" />
-                <input type="hidden" name="view" value="multi-portal" />
-                <fieldset>
-                    <select name="digilan-token-hostname" id="digilan-token-select-hostname" class="regular-text">
-                        <?php
-                        $access_points = $settings->get('access-points');
-                        foreach ($access_points as $hostname=>$content) :
-                            if (false == isset($content['specific_ap_settings'])) {
-                        ?>
-                            <option value="<?php echo $hostname; ?>"><?php echo $hostname; ?></option>
-                        <?php 
-                            } 
-                        endforeach; ?>
-                    </select>
-                    =>
-                    <select name="digilan-token-user-id" id="digilan-token-user-id" class="regular-text">
-                        <?php
-                        $users = get_users( 'role=subscriber' );
-                        foreach ( $users as $user ) :
-                        ?>
-                            <option value="<?php echo $user->ID; ?>"><?php echo $user->user_email; ?></option>
-                        <?php 
-                        endforeach; 
-                        ?>
-                    </select>
-                </fieldset>
-                <input type="submit" name="digilan_token_link_ap" class="button button-primary" value="Valider">
-            </form>
-        <?php
-        } 
-        ?>
+      <h2><?php _e('Link User to an AP', 'digilan-token'); ?></h2>
+      <?php
+      if (false == DigilanTokenMultiPortal::is_ap_available()) {
+      ?>
+        <p>
+          <?php _e('There are not ap available', 'digilan-token'); ?>
+        </p>
+      <?php
+      } else {
+      ?>
+        <form method="POST" action="<?php echo admin_url('admin-post.php'); ?>">
+          <?php wp_nonce_field('digilan-token-plugin'); ?>
+          <input type="hidden" name="digilan-token-link-ap" value="true" />
+          <input type="hidden" name="action" value="digilan-token-plugin" />
+          <input type="hidden" name="view" value="multi-portal" />
+          <fieldset>
+            <select name="digilan-token-hostname" id="digilan-token-select-hostname" class="regular-text">
+              <?php
+              $access_points = $settings->get('access-points');
+              foreach ($access_points as $hostname => $content) :
+                if (false == isset($content['specific_ap_settings'])) {
+              ?>
+                  <option value="<?php echo $hostname; ?>"><?php echo $hostname; ?></option>
+              <?php
+                }
+              endforeach; ?>
+            </select>
+            =>
+            <select name="digilan-token-user-id" id="digilan-token-user-id" class="regular-text">
+              <?php
+              $users = get_users('role=subscriber');
+              foreach ($users as $user) :
+              ?>
+                <option value="<?php echo $user->ID; ?>"><?php echo $user->user_email; ?></option>
+              <?php
+              endforeach;
+              ?>
+            </select>
+          </fieldset>
+          <input type="submit" name="digilan_token_link_ap" class="button button-primary" value="Valider">
+        </form>
+      <?php
+      }
+      ?>
     </div>
     <div id='digilan_token_generate_page_div'>
-        <h2><?php _e('Generate multi-portal page','digilan-token'); ?></h2>
-        <form method="POST" action="<?php echo admin_url('admin-post.php'); ?>">
-            <?php wp_nonce_field('digilan-token-plugin'); ?>
-            <input type="hidden" name="digilan-token-generate-pages" value="true" />
-            <input type="hidden" name="action" value="digilan-token-plugin" />
-            <input type="hidden" name="view" value="multi-portal" />
-            <p class="submit">
-                <input type="submit" name="digilan_token_generate_page" class="button button-primary" value="Generer les pages">
-            </p>
-        </form>
+      <h2><?php _e('Generate multi-portal page', 'digilan-token'); ?></h2>
+      <form method="POST" action="<?php echo admin_url('admin-post.php'); ?>">
+        <?php wp_nonce_field('digilan-token-plugin'); ?>
+        <input type="hidden" name="digilan-token-generate-pages" value="true" />
+        <input type="hidden" name="action" value="digilan-token-plugin" />
+        <input type="hidden" name="view" value="multi-portal" />
+        <p class="submit">
+          <input type="submit" name="digilan_token_generate_page" class="button button-primary" value="Generer les pages">
+        </p>
+      </form>
     </div>
-</div>
+  </div>
 <?php else : ?>
   <div class="digilan-token-activation-required">
     <h1><?php _e('Activation required', 'digilan-token'); ?></h1>
