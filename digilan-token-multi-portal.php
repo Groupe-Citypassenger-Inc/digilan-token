ed4aa7eb21f64f65a8f504989ed6c8e454c6755f
jamesjin18 <maman1000000@hotmail.fr>
2021-09-21 17:48:05 +0200

    minor change

--- digilan-token-multi-portal.php
+++ digilan-token-multi-portal.php
@@ -79,9 +79,6 @@ class DigilanTokenMultiPortal {
 
     public static function update_settings($hostname,$new_settings)
     {
-        $shared_key = ['portal-page','portal_page','landing-page','landing_page','error_page','schedule_router'];
-        
-
         $shared_settings = array_filter($new_settings, function($k) {
             $shared_key = ['portal-page','portal_page','landing-page','landing_page','error_page','schedule_router'];
             return in_array($k,$shared_key);
@@ -91,14 +88,13 @@ class DigilanTokenMultiPortal {
             $ap_key = ['timeout','ssid','country_code','access','mac'];
             return in_array($k,$ap_key);
         },ARRAY_FILTER_USE_KEY);
-
-        if (isset($shared_settings)) {
+        if (false == empty($shared_settings)) {
             $result_update_all = self::update_to_all_client_ap_settings($hostname,$shared_settings);
         }
         if (false == $result_update_all && isset($result_update_all)) {
             return false;
         }
-        if (isset($ap_settings)) {
+        if (false == empty($ap_settings)) {
             $result_update_ap = self::update_to_a_client_settings($hostname,$ap_settings);
         }
         if (false == $result_update_ap && isset($result_update_ap)) {
