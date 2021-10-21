81ebe6a8c78e5c93c60fd0c663c0bcc48bcb1ff8
jamesjin18 <maman1000000@hotmail.fr>
2021-10-21 10:58:35 +0200

    replace auth div by loader

--- js/tos-and-authenticate.js
+++ js/tos-and-authenticate.js
@@ -1,15 +1,13 @@
 (function ($) {
 	$(document).ready(function () {
-		var isClicked = false;
 		$(".dlt-auth").click(function (e) {
 			if (!$("#dlt-tos").prop("checked")) {
 				$("#dlt-tos").focus();
 				e.preventDefault();
 				return false;
 			}
-			$('.page_loader').css("display","block");
-			$(".dlt-container").css("display","none");
-			$("#dlt-gtu").css("display","none");
+			var loader = '<div class="page_loader"><img src="'+url_img+'" alt="loader" /></div>';
+			$('#dlt-center').replaceWith(loader);
 			setTimeout(function() {
 				document.location.reload();
 			}, 15000);
